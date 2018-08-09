fedc05fe4c3de1c0b8ebf33a05f5dea82993771a
Nicholas Powers <njpowers7915@gmail.com>
2018-08-09 14:09:02 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -4,8 +4,7 @@ class SongsWorker
 
   def perform(songs_file)
     CSV.foreach(songs_file, headers: true) do |lead|
-      @artist = Artist.find_or_create_by(name: song[1])
-      Song.create(title: song[0], artist_id: @artist.id)
+      Song.create(title: song[0], artist_name: song[1])
     end
   end
 end
